3512ff771469a12a846c50991838d788507eb7ed
indrabrata <indrabrata599@gmail.com>
2026-01-01 13:48:22 +0800

    feature : travel list

--- 04-state-event-forms/src/App.js
+++ 04-state-event-forms/src/App.js
@@ -8,34 +8,64 @@ const messages = [
 
 export default function App() {
   // State -> data that a component can hold over time for inforamation that it need to be remembered
+  // State is preserver throughout re renders
+  // Component will re rendered if the value of the state is changed
+  // State is actually isolated inside the component
+  // With state, we view UI as A reflection of data changing over time
+  // Dont use useState for the data that no need to trigger re render
   const [stepState, setStepState] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    if (stepState + 1 > 3) {
+      setStepState(1);
+    } else {
+      setStepState((s) => s + 1); // this is the best practice to update the state
+    }
+  }
+
+  function handleBack() {
+    if (stepState - 1 < 1) {
+      setStepState(3);
+    } else {
+      setStepState((s) => s - 1);
+    }
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${stepState === 1 ? "active" : ""}`}>1</div>
-        <div className={`${stepState === 2 ? "active" : ""}`}>2</div>
-        <div className={`${stepState === 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step : {stepState}: {messages[stepState - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${stepState >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${stepState >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${stepState >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-          onClick={() => setStepState(stepState - 1)}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-          onClick={() => setStepState(stepState + 1)}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step : {stepState}: {messages[stepState - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handleBack}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
